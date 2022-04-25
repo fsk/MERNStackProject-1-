5e486723c4737344df57c64c00497248c3bd9e1e
fsk <furkankulaksiz68@gmail.com>
2022-04-25 16:24:30 +0300

    Avatar Eklendi. Componentler Ayristirildi.

--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,8 +1,13 @@
 import React from "react";
+import UsersList from "../components/UsersList";
+
+
 
 const Users = () => {
+
+    const USERS = [{id: 'u1', name: 'FSK', image: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg', places: 8}];
     return(
-        <h1>User Work</h1>
+        <UsersList items={USERS}/> 
     );
 }
 
