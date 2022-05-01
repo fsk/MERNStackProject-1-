54fa595f3c7020f0f285651007226b151dbd46a8
fsk <furkankulaksiz68@gmail.com>
2022-05-02 01:47:06 +0300

    Add Place formu bitti.

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,19 +1,83 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/Components/FormElement/Input";
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import Button from "../../shared/Components/FormElement/Button";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 
 import './NewPlace.css';
 
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid = true;
+            for (const inputId in state.inputs) {
+                if (inputId === action.inputId) {
+                    formIsValid = formIsValid && action.isValid;
+                } else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
+            }
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.inputId]: { value: action.value, isValid: action.isValid }
+                },
+                isValid: formIsValid
+            };
+        default:
+            return state;
+    }
+};
+
 const NewPlace = () => {
+
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
+        },
+        isValid: false
+    });
+
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({ type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id })
+    }, [dispatch]);
+
+    const placeSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    }
+
+
     return (
-        <form className="place-form">
-            <Input 
-                element="input" 
-                type="text" 
-                label="title" 
-                validators={[VALIDATOR_REQUIRE()]} 
-                errorText="Please enter a valid Title"/>
+        <form className="place-form" onSubmit={placeSubmitHandler}>
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid Title"
+                onInput={inputHandler}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter a valid descriptipn. (At Least 5 Characters)"
+                onInput={inputHandler}
+            />
+            <Button type="submit" disabled={!formState.isValid}>
+                ADD PLACE
+            </Button>
         </form>
     );
 };
